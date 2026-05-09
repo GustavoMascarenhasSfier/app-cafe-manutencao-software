97fac1eb43c9bcefde22c2da350448fd99db7531
Thiago <thiagotorafrazao09@gmail.com>
2026-05-08 23:14:16 -0300

    hotfix: Adicionando conexão do GlobalStyles no main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "antd/dist/reset.css"; 
+import "antd/dist/reset.css";
+import { globalStyles } from "./styles/global";
+
+globalStyles();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
