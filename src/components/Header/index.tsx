4e7c434e142a8063c46e09e85aa3bcad8a96c7bf
Gustavo Mascarenhas Sfier <gugab1249@gmail.com>
2026-05-09 00:07:03 -0300

    feature/add-header (#11)
    Co-authored-by: Gustavo <gustavo.arando@qualitydigital.global>

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { SearchOutlined } from "@ant-design/icons";
+import { ShoppingCart } from "phosphor-react";
+import capuccino from "../../assets/Cafes/Capuccino.svg";
+import * as S from "./styles";
+
+const Header = () => {
+  const [busca, setBusca] = useState("");
+
+  return (
+    <S.PageHeader>
+      <S.LogoPlaceholder>
+        <S.LogoIcon src={capuccino} alt="Logo" />
+        <S.LogoText>Expresso <span>Delivery</span></S.LogoText>
+      </S.LogoPlaceholder>
+
+      <S.SearchContainer>
+        <S.SearchInput
+          placeholder="Buscar cafés, blends, origens..."
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
+        />
+        <S.SearchButton onClick={() => console.log(busca)}>
+          <SearchOutlined />
+        </S.SearchButton>
+      </S.SearchContainer>
+
+      <S.CartContainer>
+        <S.CartLink as={Link} to="/checkout">
+          <ShoppingCart size={22} />
+          <S.CartLabel>Carrinho</S.CartLabel>
+        </S.CartLink>
+      </S.CartContainer>
+    </S.PageHeader>
+  );
+};
+
+export default Header;
