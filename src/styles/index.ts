d247ee7f461a872713a58a273cd8e24138dd6369
Gustavo <gustavo.arando@qualitydigital.global>
2026-05-08 22:53:19 -0300

    Revert "feat: Adicionando fontSizes ao GlobalStyles"
    This reverts commit 356e7d2059fda27a9c4dd4b1d584f5103d836e23.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,36 +4,22 @@ import { createStitches } from "@stitches/react";
 export const { styled, css, globalCss, theme, config } = createStitches({
   theme: {
     colors: {
-      white: "#ffffff",
-      black: "#000000",
+      white: "#FFF",
+      black: "#000",
 
-      gray100: "#f8f4ef", // antes: bege-claro
-      gray900: "#2d2d2d", // antes: preto-fosco
+      gray100: "#E1E1E6",
+      gray200: "#A9A9B2",
+      gray400: "#7C7C8A",
+      gray500: "#505059",
+      gray600: "#323238",
+      gray700: "#29292E",
+      gray800: "#202024",
+      gray900: "#121214",
 
-      brown900: "#3b2f2f", // antes: marrom-escuro
-      brown700: "#6b4e2f", // antes: marrom-cafe
-
-      yellow500: "#f3c300", // antes: amarelo-mostarda
-
-      gold100: "#E8C07A",
-      gold200: "#C99B4E",
-      gold300: "rgba(201, 155, 78, 0.3)",
-      gold400: "rgba(201, 155, 78, 0.12)",
-    },
-    fontSizes: {
-      xxs: "0.625rem",
-      xs: "0.75rem",
-      sm: "0.875rem",
-      md: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "4xl": "2rem",
-      "5xl": "2.25rem",
-      "6xl": "3rem",
-      "7xl": "4rem",
-      "8xl": "4.5rem",
-      "9xl": "6rem",
+      ignite300: "#00B37E",
+      ignite500: "#00875F",
+      ignite700: "#015F43",
+      ignite900: "#00291D",
     },
   },
 });
