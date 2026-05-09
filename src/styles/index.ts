19fa2ec1c170ba93f65c5fa727f310b498b06154
Thiago Torá Frazão <166962151+ThiagoTora@users.noreply.github.com>
2026-05-08 23:05:26 -0300

    feat: Adicionando fontSize (#9)

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,32 +1,39 @@
-// src/styles/index.ts
 import { createStitches } from "@stitches/react";
 
-export const { styled, css, globalCss, theme, config } = createStitches({
+export const { styled, css, theme } = createStitches({
   theme: {
     colors: {
-      white: "#FFF",
-      black: "#000",
+      white: "#ffffff",
+      black: "#000000",
 
-      gray100: "#E1E1E6",
-      gray200: "#A9A9B2",
-      gray400: "#7C7C8A",
-      gray500: "#505059",
-      gray600: "#323238",
-      gray700: "#29292E",
-      gray800: "#202024",
-      gray900: "#121214",
+      gray100: "#f8f4ef",
+      gray900: "#2d2d2d",
 
-      ignite300: "#00B37E",
-      ignite500: "#00875F",
-      ignite700: "#015F43",
-      ignite900: "#00291D",
+      brown900: "#3b2f2f",
+      brown700: "#6b4e2f",
+
+      yellow500: "#f3c300",
+
+      gold100: "#E8C07A",
+      gold200: "#C99B4E",
+      gold300: "rgba(201, 155, 78, 0.3)",
+      gold400: "rgba(201, 155, 78, 0.12)",
     },
-  },
-});
 
-export const globalStyles = globalCss({
-  body: {
-    backgroundColor: "$gray900",
-    color: "$gray100",
+    fontSizes: {
+      xxs: "0.625rem",
+      xs: "0.75rem",
+      sm: "0.875rem",
+      md: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "4xl": "2rem",
+      "5xl": "2.25rem",
+      "6xl": "3rem",
+      "7xl": "4rem",
+      "8xl": "4.5rem",
+      "9xl": "6rem",
+    },
   },
 });
